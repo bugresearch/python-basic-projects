2f7e954aa7a3c5d656e0e490fd7af51edc92da24
Deniz Bektaş <cop@denizbektas.com.tr>
2021-04-23 22:04:17 +0300

    Create ikisayisorgula2.py

---
+++ ikinci-hafta-ornekleri/ikisayisorgula2.py
@@ -0,0 +1,9 @@
+x = int(input("Birinci Sayı: "))
+y = int(input("İkinci Sayı: "))
+
+if x > y:
+    print("Birinci sayı, ikinci sayıdan büyüktür.")
+elif y > x:
+    print("İkinci sayı birinci sayıdan büyüktür.")
+else:
+    print("İki sayı birbirine eşittir.")
